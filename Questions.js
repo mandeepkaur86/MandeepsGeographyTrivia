c72a4793522b0dd8b289cd27d329ea4914ba300f
mandeepkaur86 <120343040+mandeepkaur86@users.noreply.github.com>
2023-06-08 20:00:42 -0700

    initial commit

---
+++ Questions.js
@@ -0,0 +1,12 @@
+allQs = [
+    {
+        "Question":"Which is largest state in United States",
+        "Answer":"Arizona",
+        "Options":"Texas , Arizona, California, Indiana"
+    },
+    {
+        "Question":"Which is largest river in United States",
+        "Answer":"Nile",
+        "Options":"Texas , Nile, Missisippi, Indiana"
+    }
+]
