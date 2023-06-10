1c193b97ad109cd32a881963061e80954eaa2b34
mandeepkaur86 <mandeepkaurdia@gmail.com>
2023-06-09 21:26:56 -0700

    Basic Working version #1

--- Questions.js
+++ Questions.js
@@ -1,20 +1,210 @@
  
 
 
- allQs = [
+  let allQs = [
     
     
+    {
+        "Question":"Which is largest state in United States?",
+        "Answer":"Arizona",
+        "Options":"Texas,Arizona,California,Indiana"
+    },
+    
+    {
+        "Question":"What is tallest waterfall in the world?",
+        "Answer":"Angel falls",
+        "Options":"niagara falls,Iguazu alls,Victoria falls,Angel falls",
+    },
+    {
+        "Question":"What is tallest mountains in United States",
+        "Answer":"Mount Mckinley",
+        "Options":"Mount Mckinley,Mount Kilimanjaro,Himalayas,Kangchenjunga",
+    },
+    
+    {
+        "Question":"What is the world's biggest river, by volume of water discharged? United States",
+        "Answer":"Amazon",
+        "Options":"Texas,Arizona,California,Amazon"
+    },
+    {
+        "Question":"What  is the smallest country",
+        "Answer":"Vatican City",
+        "Options":"Australia,germany,Vatican city, united states"
+    },
+    {
+        "Question":"What was the biggest land empire of all time",
+        "Answer":"Mongol Empire",
+        "Options":"Mongol Empire,Russia Empire, spanish Empire, Birtish Empire"
+    },
+    {
+        "Question":"What country has the largest Muslim population",
+        "Answer":"Indonesia",
+        "Options":"India , Pakistan,Indonesia, Bangladesh"
+    },
+    
+    {
+        "Question":"What is largest population in the world",
+        "Answer":"India",
+        "Options":"Chiana,India,Pakistan,united state"
+    },
+    {
+        "Question":"What is the capital of the Pakistan",
+        "Answer":"Islamabad",
+        "Options":"Islamabad,Punjab,Sindh,Balochistan"
+    },
+    
+    {
+        "Question":"What is the nickname of Paris",
+        "Answer":"City of light",
+        "Options":"City of blue light,City of light,Star light,bright"
+    },
+    {
+        "Question":"What is the oldest University in England",
+        "Answer":"Oxfond",
+        "Options":"Washington  , Arizona, California, Oxfond"
+    },
+    {
+        "Question":"What is the world's first national park",
+        "Answer":"Yellowstone national park",
+        "Options":"Yellowstone national park , Jasper national, Yosemite national, Olympic national"
+    },
+    {
+        "Question":"Which one of the original 13 colonies doesn't border the Atlantic Ocean",
+        "Answer":"Pennsylvania",
+        "Options":"Pennsylvania , Arizona, California, Indiana"
+    },
+    
+    {
+        "Question":"Which continent is the United States located in",
+        "Answer":"North America",
+        "Options":"North America, South America, Africa,Europe"
+    },
+    {
+        "Question":"Which Ocean border the United States on the east",
+        "Answer":"Atlantic",
+        "Options":"Atlantic , Arizona, California, Indiana"
+    },
+    {
+        "Question":"What country is our neighbor to the north",
+        "Answer":"Canada",
+        "Options":"Russia,Canada,Mexico,Newzland"
+    },
+    {
+        "Question":"What country is our neighbor to the south",
+        "Answer":"Mexico",
+        "Options":"Texas , Arizona, Mexico, Indiana"
+    },
+    {
+        "Question":"What state does the Mississippi River rise",
+        "Answer":"Minnesota",
+        "Options":"Texas , Minnesota, California, Indiana"
+    },
+    {
+        "Question":"Name the capital city of the u.s",
+        "Answer":"Washington D.C",
+        "Options":"Texas , Arizona, Washington D.C, Indiana"
+    },
+    {
+        "Question":"Which Two states is Washington D.C located in",
+        "Answer":"Maryland & Virginia",
+        "Options":"Texas& Arizona, Maryland &Virginia,California& Indiana,NC & SC"
+    },
+    {
+        "Question":"Which is largest City in California",
+        "Answer":"Los Angeles & L.A",
+        "Options":"Los Angeles &L.A , Arizona& California, Indiana & Texes,Seattle & kent"
+    },
+    {
+        "Question":"Which is largest city in United States",
+        "Answer":"New York",
+        "Options":"New York , Arizona, California, Indiana"
+    },
+    {
+        "Question":"What state is largest state in United States",
+        "Answer":"Arizona",
+        "Options":"Texas , Arizona, California, Indiana"
+    },
     {
         "Question":"Which is largest state in United States",
         "Answer":"Arizona",
         "Options":"Texas , Arizona, California, Indiana"
     },
     {
-        "Question":"Which is largest river in United States",
-        "Answer":"Nile",
-        "Options":"Texas , Nile, Missisippi, Indiana"
-    }
+        "Question":"Which is largest state in United States",
+        "Answer":"Arizona",
+        "Options":"Texas , Arizona, California, Indiana"
+    },
+    {
+        "Question":"Which is largest state in United States",
+        "Answer":"Arizona",
+        "Options":"Texas , Arizona, California, Indiana"
+    },
+    {
+        "Question":"Which is largest state in United States",
+        "Answer":"Arizona",
+        "Options":"Texas , Arizona, California, Indiana"
+    },
+    {
+        "Question":"Which is largest state in United States",
+        "Answer":"Arizona",
+        "Options":"Texas , Arizona, California, Indiana"
+    },
+    {
+        "Question":"Which is largest state in United States",
+        "Answer":"Arizona",
+        "Options":"Texas , Arizona, California, Indiana"
+    },
+    {
+        "Question":"Which is largest state in United States",
+        "Answer":"Arizona",
+        "Options":"Texas , Arizona, California, Indiana"
+    },
+    {
+        "Question":"Which is largest state in United States",
+        "Answer":"Arizona",
+        "Options":"Texas , Arizona, California, Indiana"
+    },
+    {
+        "Question":"Which is largest state in United States",
+        "Answer":"Arizona",
+        "Options":"Texas , Arizona, California, Indiana"
+    },
+    {
+        "Question":"Which is largest state in United States",
+        "Answer":"Arizona",
+        "Options":"Texas , Arizona, California, Indiana"
+    },
+    {
+        "Question":"Which is largest state in United States",
+        "Answer":"Arizona",
+        "Options":"Texas , Arizona, California, Indiana"
+    },
+    {
+        "Question":"Which is largest state in United States",
+        "Answer":"Arizona",
+        "Options":"Texas , Arizona, California, Indiana"
+    },
+    {
+        "Question":"Which is largest state in United States",
+        "Answer":"Arizona",
+        "Options":"Texas , Arizona, California, Indiana"
+    },
+    {
+        "Question":"Which is largest state in United States",
+        "Answer":"Arizona",
+        "Options":"Texas , Arizona, California, Indiana"
+    },
+    {
+        "Question":"Which is largest state in United States",
+        "Answer":"Arizona",
+        "Options":"Texas , Arizona, California, Indiana"
+    },
+    
+{
+    "Question":"Which is largest river in United States",
+    "Answer":"Nile",
+    "Options":"Texas , Nile, Missisippi, Indiana"
+}
 ]
 
 
-export {allQs};
--- Trivia.js
+++ Trivia.js
@@ -1,51 +1,30 @@
 
-allQs = [
-    {
-        "Question":"Which is largest state in United States",
-        "Answer":"Arizona",
-        "Options":"Texas , Arizona, California, Indiana"
-    },
-
-    {
-        "Question":"Which is largest river in United States",
-        "Answer":"Nile",
-        "Options":"Texas,Nile,Missisippi,Indiana"
-    },
-    {
-        "Question":"Which is largest Q3 in United States",
-        "Answer":"A3",
-        "Options":"A1,A2,A3,A4"
-    },
-    {
-        "Question":"Which is largest Q4 in United States",
-        "Answer":"A4",
-        "Options":"A1,A2,A3,A4"
-    },
-    {
-        "Question":"Which is largest Q5 in United States",
-        "Answer":"A5",
-        "Options":"A1,A2,A4,A5"
-    }
-]
-
-
 let question = null;
 let score = 0;
 let totalQuestion = 0;
-let max =5;
+let max = 3;
+
+
+
+//allButtons
+let btn1 = document.getElementById("op1");
+let btn2 = document.getElementById("op2");
+let btn3 = document.getElementById("op3");
+let btn4 = document.getElementById("op4");
+let buttonPressed =  document.getElementById("op4");
 getRandomQuestion();
-
-
 function getRandomQuestion()
 {
     let random = getRandomNumber();
+    makeButtonClickable();
+    document.getElementById("face").setAttribute("src","assests/Thinking.gif");
     question = allQs[random];
-
+    console.log(question.Answer);
     document.getElementById("question").innerText = question.Question;
     let options = question.Options;
     let allOptions = options.split(",");
-    console.log(allOptions[2]);
-    document.getElementById("op1").textContent = allOptions[0];
+    
+    document.getElementById("op1").innerText = allOptions[0];
     document.getElementById("op2").innerText = allOptions[1];
     document.getElementById("op3").innerText = allOptions[2];
     document.getElementById("op4").innerText = allOptions[3];
@@ -53,40 +32,60 @@ function getRandomQuestion()
 
 }
 
-
+function makeButtonNonClickable()
+{
+    btn1.removeAttribute("onclick");
+    btn2.removeAttribute("onclick");
+    btn3.removeAttribute("onclick");
+    btn4.removeAttribute("onclick");
+}
+function makeButtonClickable()
+{
+    btn1.setAttribute("onclick","checkAnswer('op1')");
+    btn2.setAttribute("onclick","checkAnswer('op2')");
+    btn3.setAttribute("onclick","checkAnswer('op3')");
+    btn4.setAttribute("onclick","checkAnswer('op4')");
+    buttonPressed.setAttribute("style","background-color:aqua");
+}
 function checkAnswer(userButton)
 {
-    console.log(question.Answer);
-    let buttonPressed = document.getElementById(userButton);
+    console.log("Clicked");
+    buttonPressed = document.getElementById(userButton);
+    buttonPressed.setAttribute("style","background-color:cornflowerblue");
     let userAnswer = buttonPressed.innerText;
     console.log(userAnswer);
+    makeButtonNonClickable();
     if(question.Answer === userAnswer)
     {
         console.log("qual");
         score ++;
-        document.getElementById("score").innerText = score;
+        document.getElementById("score").innerText = "You Score is : "+score;
         document.getElementById("face").setAttribute("src","assests/HappyImage.jpg");
     }
     else
     {   
         console.log("not equal");
         score --;
-        document.getElementById("score").innerText = score;
+        document.getElementById("score").innerText = "You Score is : "+score;
         document.getElementById("face").setAttribute("src","assests/sadface.png");
 
     }
-    setTimeout('', 10000);
-    if(totalQuestion < max)
-        getRandomQuestion();
-    else
-        gameOver();
+   setTimeout(nextStep, 10000);
+   
 }
 
+function nextStep()
+{
+    if(totalQuestion < max)
+    getRandomQuestion();
+else
+    gameOver();
+}
 
 function getRandomNumber()
 {
    
-    let randomNumber = Math.floor(Math.random() * max);
+    let randomNumber = Math.floor(Math.random() * max);  
      console.log(randomNumber);
      return randomNumber;
       
@@ -94,8 +93,21 @@ function getRandomNumber()
 
 function gameOver()
 {
-    let gameOver = document.getElementById(gameOver);
-    console.log(gameOver);
+    let header = document.getElementById("empty");
+    let main = document.getElementById("main");
+    let image = document.getElementById("faceDiv");
+    image.innerHTML="";
+    image
+    if(score >7)
+    {
+        header.innerHTML = "GAME OVER";
+        main.innerHTML = "Good Job. You are stellar";
+    }
+    else
+    {
+        header.innerHTML = "GAME OVER";
+        main.innerHTML = "<h1> You Score is "+score+"</h1><h2> Better luck next time </h2>";
+    }
 
     //Question in different js
     //more questions
